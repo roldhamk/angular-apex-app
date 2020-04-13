e4bf0aba55ec616b0240c2ed0e3025c5502d0f40
Rachael Oldham-Knott <roldhamk@gmail.com>
2020-04-13 21:31:14 +0100

    adjusted settings for heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./dist/apexapp"));
+
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "./dist/apexapp" });
+});
+
+app.listen(process.env.PORT || 8080);
